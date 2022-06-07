290d620f8afd4b5da72fc04d3bf8b53e06b349bd
Pham Van Dan <phamvandan.cntt2@gmail.com>
2022-06-07 10:46:10 +0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, Response
+import jsonpickle
+import numpy as np
+import cv2
+
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+
+# route http posts to this method
+@app.route('/api/test', methods=['POST'])
+def test():
+    r = request
+    file = request.files['image']
+    print(file.filename)
+    # # convert string of image data to uint8
+    nparr = np.fromstring(file.read(), np.uint8)
+    # # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imwrite(file.filename, img)
+    # do some fancy processing here....
+
+    # build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+                }
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+if __name__ == "__main__":
+    # start flask app
+    app.run(host="0.0.0.0", port=5000)
